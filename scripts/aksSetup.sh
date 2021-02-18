b1534fb921176635cd65505960c892b9b4608ae9
Chesli Jones <cheslijones@gmail.com>
2021-02-18 06:03:59 -0800

    In the process of setting up secrets for development, staging, and production

--- scripts/aksSetup.sh
+++ scripts/aksSetup.sh
@@ -1,8 +1,6 @@
-az group create -l westus -n k8s-tut && \
-az acr create -g k8s-tut -n k8stutacr --sku Basic -l westus && \
-az keyvault create --name k8stutakv --resource-group k8s-tut -l westus && \
-az keyvault secret set --vault-name k8stutakv --name SECRETTEST --value abc123 && \
-az aks create -n k8stutaks -g k8s-tut --kubernetes-version=1.19.7 --node-count 1 -l westus --enable-managed-identity --attach-acr k8stutacr -s Standard_B2s --network-plugin azure && \
-az aks get-credentials -n k8stutaks -g k8s-tut && \
-helm repo add csi-secrets-store-provider-azure https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts && \
-helm install csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --generate-name
+az group create -l westus -n k8s-tut
+az acr create -g k8s-tut -n k8stutacr --sku Basic -l westus
+az keyvault create --name k8stutakv --resource-group k8s-tut -l westus
+az keyvault secret set --vault-name k8stutakv --name SECRETTEST --value abc123
+az aks create -n k8stutaks -g k8s-tut --kubernetes-version=1.19.7 --node-count 1 -l westus --enable-managed-identity --attach-acr k8stutacr -s Standard_B2s --network-plugin azure
+az aks get-credentials -n k8stutaks -g k8s-tut
---
+++ scripts/akvIntegrationSetup.sh
@@ -0,0 +1,2 @@
+helm repo add csi-secrets-store-provider-azure https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts && \
+helm install csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --generate-name
